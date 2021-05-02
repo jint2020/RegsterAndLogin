c1c6b31d5ccac4be2f1e05d4d92971b118645002
Jin Tang <jtan0213@student.monash.edu>
2021-05-03 02:00:53 +0800

    final version

--- src/App.java
+++ src/App.java
@@ -3,33 +3,93 @@ import java.util.Scanner;
 
 
 public class App {
+    private final FileHandler fh;
+
+    public App(){
+        fh = new FileHandler();
+    }
+
+    //菜单
+    public void menu(){
+        System.out.println("===============MAIN MENU==============");
+        System.out.println("(1) Register");
+        System.out.println("(2) Login");
+        System.out.println("(3) Exit the system");
+        System.out.println("Enter an option:\n");
+    }
+
+    // 用户输入选择功能
+    public void program() {
+        menu();
+        Scanner input = new Scanner(System.in);
+        String option = input.nextLine();
+
+        while (!option.equals("3")) {
+            if (option.equals("1"))
+                registerUser();
+            else if (option.equals("2"))
+                loginUser();
+            else {
+                System.out.println("[Wrong Choice] ");
+                System.out.println("[You must select 1 - 3]");
+            }
+            menu();
+            option = input.nextLine();
+        }
+        System.out.println("System exit");
+    }
+
+    // 用户注册功能
+    public void registerUser(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your username:");
+        //存储输入的用户名
+        String userName = input.nextLine();
+        System.out.println("Please enter your password: ");
+        //存储输入的密码
+        String password = input.nextLine();
+
+
+        //存储文件路径
+        String filePath = "./files/user.csv";
+        //调用checkFileExist方法检测文件是否存在
+        fh.checkFileExist(filePath);
+
+        //调用用户注册方法检验用户信息是否存在
+        if(fh.regFunc(userName, password, filePath)) {
+            System.out.println("Completed registration\n");
+        }else {
+            System.out.println("Username has been taken\n");
+        }
+    }
+
+    // 用户登录功能
+    public void loginUser(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your username:");
+        //存储输入的用户名
+        String userName = input.nextLine();
+        System.out.println("Please enter your password: ");
+        //存储输入的密码
+        String password = input.nextLine();
+        //存储文件路径
+        String filePath = "./files/user.csv";
+        fh.checkFileExist(filePath);
+
+        //调用用户登录验证方法检验用户信息是否匹配
+        if(fh.valUserDetail(userName, password, filePath)) {
+            System.out.println("Login successfully\n");
+        } else if (!fh.searchPassStr("pw:" + password,filePath)){
+            System.out.println("Wrong password\n");
+        }else {
+            System.out.println("User does not exist\n");
+        }
+    }
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-
-        String flag = String.valueOf(in.nextLine());		//标志变量，区分注册或登录
-        System.out.println("Please enter your username:");
-        String userName = in.nextLine();			//存储输入的用户名
-        System.out.println("Please enter your password: ");
-        String password = in.nextLine();			//存储输入的密码
-
-        in.close();
-
-        String fileUrl = "./files/user.csv";			//存储文件路径
-        FileHandler.FileExistTest(fileUrl);			//检测文件是否存在，若不存在，则在指定路径新建
-
-        if(flag.equalsIgnoreCase("reg") ) {									//flag为0，注册操作
-            if(FileHandler.UserRegister(userName, password, fileUrl)) {		//调用用户注册函数
-                System.out.println("注册成功");
-            }else {
-                System.out.println("用户名已存在");
-            }
-        }else if(flag.equalsIgnoreCase("log") ) {										//flag为1，登录操作
-            if(FileHandler.UserValidate(userName, password, fileUrl)) {		//调用用户登录验证函数
-                System.out.println("登录成功");
-            }else {
-                System.out.println("用户名或密码错误");
-            }
-        }
+        App run = new App();
+        run.program();
     }
 }
--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,5 +1,5 @@
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -9,137 +9,108 @@ import java.io.InputStreamReader;
 
 public class FileHandler {
 
-    /*
-     * 函数名：FileExistTest
-     * 功能描述：检查本地存储用户名和密码的文件是否已存在，若不存在，则创建新文件
-     * 输入：String fileUrl--需要检查的文件路径（存储本地用户名和密码的文件）
-     * 输出：若文件已存在或（不存在但新建文件成功），则返回true；否则返回false
-     * */
-    static boolean FileExistTest(String fileUrl) {
-        boolean out = false;
+    // 检查文件是否存在
+    // 传入filePath
+    public boolean checkFileExist(String filePath) {
+        boolean flag = false;
         try {
-            File f = new File(fileUrl);
-            if(!f.exists()) {						//若文件不存在
-                f.createNewFile();					//新建该文件
-
-                String sets=" attrib +H  "+f.getAbsolutePath(); 	//设置文件属性为隐藏
-                Runtime.getRuntime().exec(sets);			//执行文件设置
-                out = true;
+            File f = new File(filePath);
+            //若文件不存在
+            if(!f.exists()) {
+                //新建文件
+                f.createNewFile();
+                flag = true;
             }
-            out = true;
-        } catch (IOException e) {
+            flag = true;
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
-        return out;
+        return flag;
     }
 
-    /*
-     * 函数名：StringSearchLine
-     * 功能描述：逐行读取路径为fileUrl的文件，查找字符串strDst是否存在
-     * 输入：String strDst--需要查询的目标字符串
-     * 		 String fileUrl--需要访问的文件路径
-     * 输出：若在文件中查询到目标字符串strDst，则返回true；否则返回flase
-     * */
 
-    static boolean StringSearchLine(String strDst,String fileUrl) {
-        boolean out = false;								//定义布尔类型的返回值
+    // 向目标文件（filePath）写入字符串inStr
+    // 传入inStr，filePath
+    public boolean writeStr(String inStr,String filePath) {
+        boolean flag = false;
         try {
-            FileInputStream fis = new FileInputStream(fileUrl);
-            InputStreamReader isr = new InputStreamReader(fis,"UTF-8");
-            BufferedReader br = new BufferedReader(isr);				//至此，变量br与文件中的字符流已建立连接
+            BufferedWriter out = new BufferedWriter(new FileWriter(filePath,true));
+            //向文件中追加新的字符串passStr
+            out.write("\n"+inStr);
+            out.close();
+            flag = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
 
-            String line = "";							//暂存br中取出的每一行字符串
-            while((line = br.readLine()) != null) {				//文件未读取完毕
-                if(line.equals(strDst)) {					//若找到匹配的字符串
-                    out = true;						//则返回值赋值为true，终止循环
+    // 校验用户名和密码
+    // 传入userName，password 与user.csv存的un,pw做比较
+    public boolean valUserDetail(String userName,String password,String filePath) {
+        boolean flag = false;
+        try {
+            // 读取csv文件，BufferedReader提供readLine功能
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath),"UTF-8"));
+            //暂存br中取出的每一行字符串
+            String lines = "";
+
+            //若文件未读取完毕，则继续读取下一行
+            while((lines=br.readLine()) != null) {
+                //若当前行与用户名相匹配
+                if(lines.equals("id:" + userName)) {
+                    //看密码是否匹配
+                    if(br.readLine().equals("pw:"+password)) {
+                        flag = true;
+                    }
+                    //username不匹配即停止loop
                     break;
                 }
             }
-
-            br.close();								//结束对文件的访问
-            isr.close();
-            fis.close();
-        } catch (Exception e) {							//处理异常（输出异常）
-            e.printStackTrace();
-        }
-        return out;									//返回查询结果
-    }
-
-    /*
-     * 函数名：StringWriteLine
-     * 功能描述：向路径为fileUrl的文件中写入（追加方式）字符串strDIn
-     * 输入：String strIn--需要向文件中写入（追加方式）的字符串
-     * 		 String fileUrl--需要访问的文件路径
-     * 输出：若向文件写入字符串strIn成功，则返回true；否则返回flase
-     * */
-    static boolean StringWriteLine(String strIn,String fileUrl) {
-        boolean out = false;
-        try {
-            FileWriter fw = new FileWriter(fileUrl,true);				//建立文件写入访问连接
-
-            fw.write("\n"+strIn);							//向文件中追加新的字符串StrIn
-            fw.close();								//结束对文件的访问
-
-            out = true;
+            br.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return out;
+        return flag;
     }
 
-    /*
-     * 函数名：UserValidate
-     * 功能描述：校验登录操作的用户名userName和密码password是否匹配
-     * 输入：String userName--登录操作输入的用户名
-     * 		 String password--登录操作输入的密码
-     * 		 String fileUrl--需要访问的文件路径（存储本地用户名和密码的文件）
-     * 输出：若输入的userName和password相匹配，则返回true；否则返回false
-     * */
-    static boolean UserValidate(String userName,String password,String fileUrl) {
-        boolean out = false;
+    // 查找输入的字符串
+    public boolean searchPassStr(String passStr,String filePath) {
+        boolean flag = false;
         try {
-            FileInputStream fis = new FileInputStream(fileUrl);
-            InputStreamReader isr = new InputStreamReader(fis,"UTF-8");
-            BufferedReader br = new BufferedReader(isr);				//至此，变量br与文件中的字符流建立连接
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath),"UTF-8"));
+            String line = "";
 
-            String line = "";							//暂存br中取出的每一行字符串
-
-            while((line=br.readLine()) != null) {					//若文件未读取完毕，则继续读取下一行
-                if(line.equals("un:" + userName)) {				//若当前行与用户名相匹配
-                    if(br.readLine().equals(String.valueOf(("pw:"+password).hashCode()))) {		//看密码是否匹配
-                        out = true;									//若密码匹配，返回true
-                    }
-                    break;											//只要检索到用户名，不论密码是否匹配，都终止循环
+            //文件未读取完毕
+            while((line = br.readLine()) != null) {
+                //若找到匹配的字符串
+                if(line.equals(passStr)) {
+                    //则返回值赋值为true，终止循环
+                    flag = true;
+                    break;
                 }
             }
-
-            br.close();												//关闭对文件的访问
-            isr.close();
-            fis.close();
-
+            br.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return out;
+        // 返回查询结果
+        return flag;
     }
 
-    /*
-     * 函数名：UserRegister
-     * 功能描述：以注册操作输入的用户名userName和密码password注册新用户（向存储本地用户名和密码的文件中写入新的用户名的和密码）
-     * 输入：String userName--注册操作输入的用户名
-     * 		 String password--注册操作输入的密码
-     * 		 String fileUrl--需要访问的文件路径（存储本地用户名和密码的文件）
-     * 输出：若新用户注册成功，则返回true；否则返回false
-     * */
-    static boolean UserRegister(String userName,String password,String fileUrl) {
-        boolean out = false;
-        if(StringSearchLine("un:" + userName, fileUrl)) {				//检验用户名是否已存在
-            return out;
-        }else {										//若不存在，依次写入用户名和密码（分别加上前缀"un"和"pw"）
-            StringWriteLine("un:" + userName, fileUrl);
-            StringWriteLine(String.valueOf(("pw:" + password).hashCode()), fileUrl);
-            out = true;
+    // 用户选择注册功能时输入的username和password，向user.csv写入username和password
+    public boolean regFunc(String userName,String password,String filePath) {
+        boolean flag = false;
+        //检验用户名是否已存在
+        if(searchPassStr("id:" + userName, filePath)) {
+            return flag;
+        }else {
+            //若不存在，依次写入用户名和密码（分别加上前缀"un:"和"pw:"）
+            writeStr("id:" + userName, filePath);
+            writeStr("pw:" + password, filePath);
+            flag = true;
         }
-        return out;
+        return flag;
     }
 }
